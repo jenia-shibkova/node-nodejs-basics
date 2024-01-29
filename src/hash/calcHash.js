6b995557101b27e7c502c97ff5e71d3a5fec3d76
Eugenia Shibkova <eugeniashibkova@eugenias-mbp.home>
2024-01-29 22:59:05 +0100

    feat: implement hash block

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { readFile } from 'fs/promises';
+import { createHash } from 'crypto';
+
+const hash = createHash('sha256');
+const FILE_PATH = fileURLToPath(import.meta.url);
+const DIRECTORY_PATH = path.dirname(FILE_PATH);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = await readFile(`${DIRECTORY_PATH}/files/fileToCalculateHashFor.txt`,
+    {
+      encoding: 'UTF-8',
+    }
+  );
+
+  hash.update(file);
+  console.log(hash.digest('hex'));
 };
 
 await calculateHash();
