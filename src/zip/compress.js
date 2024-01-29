4fcb15116537335d0ed3b8eab5236c083df1f0c1
Eugenia Shibkova <eugeniashibkova@eugenias-mbp.home>
2024-01-29 23:07:03 +0100

    feat: implement Zlib block

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
+const FILE_PATH = fileURLToPath(import.meta.url);
+const DIRECTORY_PATH = path.dirname(FILE_PATH);
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+
+  const readableStream = createReadStream(`${DIRECTORY_PATH}/files/fileToCompress.txt`);
+  const writableStream = createWriteStream(`${DIRECTORY_PATH}/files/archive.gz`);
+
+  readableStream.pipe(gzip).pipe(writableStream);
 };
 
 await compress();
