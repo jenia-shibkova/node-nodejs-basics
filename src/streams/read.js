ca5b4be9fb5184a5c08c184b197852f845b07642
Eugenia Shibkova <eugeniashibkova@eugenias-mbp.home>
2024-01-29 23:20:33 +0100

    feat: implement stream block

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+
+const FILE_PATH = fileURLToPath(import.meta.url);
+const DIRECTORY_PATH = path.dirname(FILE_PATH);
+
 const read = async () => {
-    // Write your code here 
+  const stream = createReadStream(`${DIRECTORY_PATH}/files/fileToRead.txt`);
+  stream.pipe(process.stdout);
 };
 
 await read();
